56e4ce48dfdec6c0c930bd6b878a336ffc009ddd
63104 <631041490@qq.com>
2020-09-01 19:57:47 +0800

    初始化young项目 ---温家荣 2020/09/01

---
+++ young-auth/src/main/java/org/springyoung/auth/AuthApp.java
@@ -0,0 +1,21 @@
+package org.springyoung.auth;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: auth
+ * @Description: 认证授权中心
+ * @Author: 温家荣-wjr
+ * @Date: 2020/9/01 15:54
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class AuthApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApp.class,args);
+    }
+
+}
