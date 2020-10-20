862a889c3e7ea5e50f16ec662e993ec436dd8665
63104 <631041490@qq.com>
2020-10-20 17:17:59 +0800

    上传数据库

--- db/young_cloud.sql
+++ db/young_cloud.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 20/10/2020 15:39:28
+ Date: 20/10/2020 17:17:33
 */
 
 SET NAMES utf8mb4;
@@ -23,6 +23,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `young_dept`;
 CREATE TABLE `young_dept`  (
   `id` bigint(64) NOT NULL AUTO_INCREMENT COMMENT '部门ID',
+  `tenant_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户',
   `parent_id` bigint(64) NOT NULL COMMENT '上级部门ID',
   `dept_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '部门名称',
   `order_num` double(20, 0) NULL DEFAULT NULL COMMENT '排序',
@@ -33,7 +34,6 @@ CREATE TABLE `young_dept`  (
   `create_user` bigint(64) NULL DEFAULT NULL COMMENT '创建人',
   `update_user` bigint(64) NULL DEFAULT NULL COMMENT '修改人',
   `is_deleted` smallint(2) NULL DEFAULT NULL COMMENT '软删除 0未删除 1删除',
-  `tenantId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户',
   `create_dept` bigint(64) NULL DEFAULT NULL COMMENT '创建部门',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 124 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '部门表' ROW_FORMAT = Dynamic;
@@ -41,7 +41,7 @@ CREATE TABLE `young_dept`  (
 -- ----------------------------
 -- Records of young_dept
 -- ----------------------------
-INSERT INTO `young_dept` VALUES (123, 0, '开发部', 1, '0', '2020-09-22 10:33:12', '2020-09-22 10:33:10', '开发部门', 10000, 10000, 0, '000000', NULL);
+INSERT INTO `young_dept` VALUES (123, '000000', 0, '开发部', 1, '0', '2020-09-22 10:33:12', '2020-09-22 10:33:10', '开发部门', 10000, 10000, 0, NULL);
 
 -- ----------------------------
 -- Table structure for young_menu
@@ -64,7 +64,6 @@ CREATE TABLE `young_menu`  (
   `create_user` bigint(64) NULL DEFAULT NULL COMMENT '创建人',
   `update_user` bigint(64) NULL DEFAULT NULL COMMENT '修改人',
   `is_deleted` smallint(2) NULL DEFAULT NULL COMMENT '软删除 0未删除 1删除',
-  `tenantId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户',
   `create_dept` bigint(64) NULL DEFAULT NULL COMMENT '创建部门',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
@@ -72,11 +71,11 @@ CREATE TABLE `young_menu`  (
 -- ----------------------------
 -- Records of young_menu
 -- ----------------------------
-INSERT INTO `young_menu` VALUES (1, 0, '系统管理', '/system', 'Layout', NULL, 'el-icon-set-up', '0', 1, '0', '2017-12-27 16:39:07', '2019-07-20 16:19:04', '菜单', 10000, 10000, 0, '000000', NULL);
-INSERT INTO `young_menu` VALUES (2, 1, '用户管理', '/system/user', 'young/system/user/Index', 'user:view', '', '0', 1, '0', '2017-12-27 16:47:13', '2019-01-22 06:45:55', '菜单', 10000, 10000, 0, '000000', NULL);
-INSERT INTO `young_menu` VALUES (3, 2, '新增用户', '', '', 'user:add', NULL, '1', NULL, '0', '2017-12-27 17:02:58', NULL, '按钮', 10000, 10000, 0, '000000', NULL);
-INSERT INTO `young_menu` VALUES (4, 2, '修改用户', '', '', 'user:update', NULL, '1', NULL, '0', '2017-12-27 17:04:07', NULL, '按钮', 10000, 10000, 0, '000000', NULL);
-INSERT INTO `young_menu` VALUES (5, 2, '删除用户', '', '', 'user:delete', NULL, '1', NULL, '0', '2017-12-27 17:04:58', NULL, '按钮', 10000, 10000, 0, '000000', NULL);
+INSERT INTO `young_menu` VALUES (1, 0, '系统管理', '/system', 'Layout', NULL, 'el-icon-set-up', '0', 1, '0', '2017-12-27 16:39:07', '2019-07-20 16:19:04', '菜单', 10000, 10000, 0, NULL);
+INSERT INTO `young_menu` VALUES (2, 1, '用户管理', '/system/user', 'young/system/user/Index', 'user:view', '', '0', 1, '0', '2017-12-27 16:47:13', '2019-01-22 06:45:55', '菜单', 10000, 10000, 0, NULL);
+INSERT INTO `young_menu` VALUES (3, 2, '新增用户', '', '', 'user:add', NULL, '1', NULL, '0', '2017-12-27 17:02:58', NULL, '按钮', 10000, 10000, 0, NULL);
+INSERT INTO `young_menu` VALUES (4, 2, '修改用户', '', '', 'user:update', NULL, '1', NULL, '0', '2017-12-27 17:04:07', NULL, '按钮', 10000, 10000, 0, NULL);
+INSERT INTO `young_menu` VALUES (5, 2, '删除用户', '', '', 'user:delete', NULL, '1', NULL, '0', '2017-12-27 17:04:58', NULL, '按钮', 10000, 10000, 0, NULL);
 
 -- ----------------------------
 -- Table structure for young_role
@@ -84,6 +83,7 @@ INSERT INTO `young_menu` VALUES (5, 2, '删除用户', '', '', 'user:delete', NU
 DROP TABLE IF EXISTS `young_role`;
 CREATE TABLE `young_role`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `tenant_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户',
   `role_name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名称',
   `status` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '业务状态',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
@@ -92,7 +92,6 @@ CREATE TABLE `young_role`  (
   `create_user` bigint(64) NULL DEFAULT NULL COMMENT '创建人',
   `update_user` bigint(64) NULL DEFAULT NULL COMMENT '修改人',
   `is_deleted` smallint(2) NULL DEFAULT NULL COMMENT '软删除 0未删除 1删除',
-  `tenantId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户',
   `create_dept` bigint(64) NULL DEFAULT NULL COMMENT '创建部门',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
@@ -100,7 +99,7 @@ CREATE TABLE `young_role`  (
 -- ----------------------------
 -- Records of young_role
 -- ----------------------------
-INSERT INTO `young_role` VALUES (1, '超级管理员', '0', '2020-09-22 10:22:58', '2020-09-22 10:22:55', '超级管理员角色', 10000, 10000, 0, '000000', NULL);
+INSERT INTO `young_role` VALUES (1, '000000', '超级管理员', '0', '2020-09-22 10:22:58', '2020-09-22 10:22:55', '超级管理员角色', 10000, 10000, 0, NULL);
 
 -- ----------------------------
 -- Table structure for young_role_menu
@@ -114,7 +113,7 @@ CREATE TABLE `young_role_menu`  (
 -- ----------------------------
 -- Records of young_role_menu
 -- ----------------------------
-INSERT INTO `young_role_menu` VALUES (1, 3);
+INSERT INTO `young_role_menu` VALUES (1, 4);
 
 -- ----------------------------
 -- Table structure for young_user
@@ -122,6 +121,7 @@ INSERT INTO `young_role_menu` VALUES (1, 3);
 DROP TABLE IF EXISTS `young_user`;
 CREATE TABLE `young_user`  (
   `id` bigint(64) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `tenant_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户',
   `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
   `password` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
   `email` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
@@ -137,7 +137,6 @@ CREATE TABLE `young_user`  (
   `create_user` bigint(64) NULL DEFAULT NULL COMMENT '创建人',
   `update_user` bigint(64) NULL DEFAULT NULL COMMENT '修改人',
   `is_deleted` smallint(2) NULL DEFAULT NULL COMMENT '软删除 0未删除 1删除',
-  `tenantId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户',
   `create_dept` bigint(64) NULL DEFAULT NULL COMMENT '创建部门',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 10001 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
@@ -145,7 +144,8 @@ CREATE TABLE `young_user`  (
 -- ----------------------------
 -- Records of young_user
 -- ----------------------------
-INSERT INTO `young_user` VALUES (10000, 'wenjiarong', '$2a$10$gzhiUb1ldc1Rf3lka4k/WOoFKKGPepHSzJxzcPSN5/65SzkMdc.SK', '631041490@qq.com', '123456789', '2020-09-22 10:20:47', '0', 123, NULL, '1', '2020-09-22 10:21:24', '2020-09-22 10:21:06', '超级管理员', 10000, 10000, 0, '000000', NULL);
+INSERT INTO `young_user` VALUES (10000, '000000', 'wenjiarong', '$2a$10$gzhiUb1ldc1Rf3lka4k/WOoFKKGPepHSzJxzcPSN5/65SzkMdc.SK', '631041490@qq.com', '123456789', '2020-09-22 10:20:47', '0', 123, NULL, '1', '2020-09-22 10:21:24', '2020-09-22 10:21:06', '超级管理员', 10000, 10000, 0, NULL);
+INSERT INTO `young_user` VALUES (1318470775754153986, NULL, 'wenjia', '$2a$10$DnxSYKmlhiwNThTZdfCAFuBe4UdHjVzZW//iMNIZ0TKmvfvn7w9Gu', NULL, NULL, NULL, '2', NULL, 'default.jpg', '1', NULL, '2020-10-20 16:35:04', NULL, NULL, NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for young_user_role
@@ -160,5 +160,6 @@ CREATE TABLE `young_user_role`  (
 -- Records of young_user_role
 -- ----------------------------
 INSERT INTO `young_user_role` VALUES (10000, 1);
+INSERT INTO `young_user_role` VALUES (1318470775754153986, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
